623ff32fd5b6f17fa2a2a887cd9a73274e57ce24
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-17 11:39:42 +0200

    Exercitii cu vectori.

---
+++ exercitii-vectori/exercitii-vectori/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercitii_vectori
+{
+    class Program
+    {
+        static int[] v;
+        static int n;
+        static Random rnd = new Random();
+
+        static void read()
+        {
+            Console.Write("Introduceti nr elementelor vectorului : ");
+            n = int.Parse(Console.ReadLine());
+            v = new int[n];
+            for (int i = 0; i < n; i++)
+                v[i] = rnd.Next(-100,101);
+        }
+
+        static void view()
+        {
+            Console.WriteLine($"Vectorul din {n} elemente:");
+            for (int i = 0; i < n; i++)
+                Console.Write(v[i] + " ");
+            Console.WriteLine();
+        }
+
+        static void ord()
+        {
+            int aux;
+            for(int i=0;i<n-1;i++)
+                for(int j=i+1;j<n;j++)
+                {
+                    if(v[i]<v[j])
+                    {
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+        }
+        static void Main(string[] args)
+        {
+            read();
+            view();
+            ord();
+            view();
+            Console.ReadKey();
+        }
+    }
+}
